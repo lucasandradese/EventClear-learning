8dc18b5df3676075aa2e74a7bcc13f9b454e84f3
lucasandradese <lucasandradese@outlook.com>
2025-11-09 10:31:07 -0300

    feat: Put context in usecases CreateEvent and SearchEvent

---
+++ src/main/java/com/dev/andradelucas/EventClear/infrastructure/beans/BeanConfiguration.java
@@ -0,0 +1,21 @@
+package com.dev.andradelucas.EventClear.infrastructure.beans;
+import com.dev.andradelucas.EventClear.core.entities.Event;
+import com.dev.andradelucas.EventClear.core.gateway.EventGateway;
+import com.dev.andradelucas.EventClear.core.usecases.*;
+import lombok.experimental.Delegate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfiguration {
+
+    @Bean
+    public CreateEventCase createEventCase (EventGateway eventGateway){
+        return new CreateEventCaseImpl(eventGateway);
+    }
+
+    @Bean
+    public SearchEventCase searchEventCase(EventGateway eventGateway){
+        return new SearchEventCaseImpl(eventGateway);
+    }
+}
